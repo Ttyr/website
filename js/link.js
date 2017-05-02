51c40304e72e7a62bd74d6cce6dd6091915cb4db
tyr <158199884@qq.com>
2017-05-02 13:08:29 +0800

    add js

---
+++ js/link.js
@@ -0,0 +1,9 @@
+function showmenu(abc){
+	var subdiv=document.getElementById(abc);
+	subdiv.style.display="block";
+	
+}
+function hidemenu(abc){
+	var subdiv=document.getElementById(abc);
+	subdiv.style.display="none";
+}
